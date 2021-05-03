a8b6513c85410cf37a6f7685957a34ffc389ec75
Navin Pathak <navin.pathak@a1technology.com>
2021-05-04 00:00:17 +0530

    Form.IO Test App

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,28 +9,11 @@ export default class MainController {
 		$scope.testing = true;
 		$scope.testmodel = [{ id: 1 }];
 		$scope.testdata = [
-			{ id: 1, label: 'David' },
-			{ id: 2, label: 'Jhon' },
-			{ id: 3, label: 'Danny' },
-		];
-		$scope.testsettings = {
-			selectionLimit: 1,
-			selectedToTop: true,
-			idProperty: 'id',
-		};
-		$scope.testevents = {
-			onSelectionChanged() { // This event is not firing on selection of max limit
-				$log.debug('you changed selection');
-			},
-		};
-
-		$scope.example1model = [];
-		$scope.example1data = [
-			{ id: admins, label: 'Administrators' },
-			{ id: authors, label: 'Authors' },
-			{ id: consumers, label: 'Consumers' },
-		];
-
+			{ role_id: admins, role: 'Administrators', role_description: 'Can Manage Content and Users', created_date: '2020-02-19T23:54:28.130Z' },
+			{ role_id: authors, role: 'Authors', role_description: '', created_date: '2020-02-19T23:54:28.204Z' },
+			{ role_id: consumers, role: 'Consumers', role_description: 'Can Execute Reports and Dashboards', created_date: '2020-02-19T23:54:28.130Z' },
+			];
+	
 		$scope.smartButtonTextProviderSettings = {
 			smartButtonTextProvider(selectionArray) {
 				return selectionArray.length + 2;
