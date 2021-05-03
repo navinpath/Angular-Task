56257d31b711725bd8f753d6baae0619390662b2
Navin Pathak <navin.pathak@a1technology.com>
2021-05-04 02:25:25 +0530

    Form.IO Test App

--- src/ng-multiselect-dropdown/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { MultiSelectComponent } from './multiselect.component';
-export { ClickOutsideDirective } from './click-outside.directive';
-export { ListFilterPipe } from './list-filter.pipe';
-export { NgMultiSelectDropDownModule } from './ng-multiselect-dropdown.module';
-export { IDropdownSettings } from './multiselect.model'
